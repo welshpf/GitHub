dfcc2faaf2cf5a8b475289aea9e852ef1189cdab
welshpf <welshpf@gmail.com>
2025-11-30 11:27:36 -0500

    Adding dataframe

--- test.R
+++ test.R
@@ -1,1 +1,8 @@
 library(tidyverse)
+df <- datasets::airmiles %>%
+      tibble::enframe(name = "year", value = "miles")
+
+# Create histogram
+df %>% 
+ggplot(aes(x = miles)) +
+geom_histogram(bins = 30)
